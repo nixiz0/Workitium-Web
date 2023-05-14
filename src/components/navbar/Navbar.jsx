3ab3ae405a3adbda0cb5ef888a1dcefb0ceb306f
Julian <julian.57@outlook.fr>
2023-05-14 16:36:04 +0200

    CTRL-Z

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './navbar.css'
 import logo_workitium from './design logo Workitium color inverse.png'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav class="nav-logo_custom">
-        <a href="/" class="logo_custom"> 
+        <Link to="/" class="logo_custom"> 
             <img src={logo_workitium} alt="LOGO"/>
             <h2> Workitium </h2>
-        </a>
+        </Link>
         <ul>
-          <li><a href="/documentation">Documentation</a></li>
+          <li><Link to="/documentation">Documentation</Link></li>
           <li><a href="/composants">Composants</a></li>
         </ul>
     </nav>
