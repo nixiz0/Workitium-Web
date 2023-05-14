7217195ceada98af9755f2fa2e4480525ffa1df5
Julian <julian.57@outlook.fr>
2023-05-14 19:29:27 +0200

    Improve docunav function for scrolling

--- src/components/documentation_navbar/DocuNav.jsx
+++ src/components/documentation_navbar/DocuNav.jsx
@@ -6,14 +6,14 @@ function DocuNav() {
   window.onscroll = function() {
     if (window.innerWidth < 599) return;
     var scroll_nav = document.getElementById("dynamic_nav");
-    if (window.pageYOffset > 93) {
+    if (scroll_nav && window.pageYOffset > 93) {
         scroll_nav.style.top = "0%";
         scroll_nav.style.backgroundColor = "#292d31";
         scroll_nav.style.padding = "1rem 1rem 1rem 1rem";
         scroll_nav.style.width = "100%";
         scroll_nav.style.boxShadow = "0px 0px 3px 3px rgba(0,0,0,0.4)"
     }
-    if (window.pageYOffset < 93) {
+    if (scroll_nav && window.pageYOffset < 93) {
         scroll_nav.style.top = "12%";
         scroll_nav.style.backgroundColor = "transparent";
         scroll_nav.style.padding = "0em";
